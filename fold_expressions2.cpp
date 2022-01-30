04240bd420b701f75ab6020f300caf3d107a842a
Yvan Pearson <yvan.pearson@gmail.com>
2022-01-30 14:34:33 -0500

    add

--- fold_expressions2.cpp
+++ fold_expressions2.cpp
@@ -13,6 +13,19 @@ constexpr T mathFunction(T x)
     return x * 543.432 + 5.2410;
 }
 
+template<typename T, std::size_t... Is>
+constexpr std::array<T, sizeof...(Is)>
+make_inc_array_impl(std::index_sequence<Is...>)
+{
+	return {{(Is + 30.0)...}};
+}
+
+template <typename T, std::size_t N>
+constexpr std::array<T, N> make_inc_array()
+{
+	return make_inc_array_impl<T>(std::make_index_sequence<N>{});
+}
+
 template <typename T, std::size_t N, T (*F)(T)>
 struct Data
 {
@@ -39,27 +52,11 @@ struct Data1
     }
 };
 
-template<typename T, std::size_t... Is>
-constexpr std::array<T, sizeof...(Is)>
-make_inc_array_impl(std::index_sequence<Is...>)
-{
-	return {{(Is + 1.5)...}}; // Adding 1.5 just so the output shows doubles instead of suppressing the decimals because the values are whole numbers
-}
-
-template <typename T, std::size_t N>
-constexpr std::array<T, N> make_inc_array()
-{
-	return make_inc_array_impl<T>(std::make_index_sequence<N>{});
-}
-
 int main()
 {
-
     constexpr auto a = make_inc_array<double, 10>(); // [1, 2, ..., 10]
 
     constexpr Data1<int, 5> b{1, 2, 3, 4, 5};
-    // constexpr auto x = std::make_integer_sequence<int, 5>{};
-    // constexpr Data1<int, 5> b{x};
 
     constexpr std::size_t data_len{10};
     constexpr Data<double, data_len, mathFunction> data{30.0, 40.0, 50.0, 60.0, 70.0, 80.0, 90.0, 100.0, 110.0, 120.0};
