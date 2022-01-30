dc39e29173f1a3f4db926b960368d2a9e78b0774
Yvan Pearson <yvan.pearson@gmail.com>
2022-01-30 13:46:37 -0500

    add

--- fold_expressions2.cpp
+++ fold_expressions2.cpp
@@ -8,15 +8,13 @@ using namespace std;
 template <typename T>
 constexpr T mathFunction(T x)
 {
-    return x * 10.0 + 25.0;
+    return x * 543.432 + 5.2410;
 }
 
 template <typename T, std::size_t N, T (*F)(T)>
 struct Data
 {
     std::array<T, N> values{};
-    std::size_t i = 0;
-
     template <typename... Args>
     constexpr Data(Args &&...pack)
         : values{F(std::forward<Args>(pack))...}
@@ -42,7 +40,6 @@ int main()
     constexpr auto a = make_inc_array<10>(); // [1, 2, ..., 10]
 
     constexpr std::size_t data_len{10};
-
     constexpr Data<double, data_len, mathFunction> data{30.0, 40.0, 50.0, 60.0, 70.0, 80.0, 90.0, 100.0, 110.0, 120.0};
 
     for (size_t i{}; i < data.values.size(); i++)
