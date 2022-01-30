972e9d1ba774fc001f9f43b5cb2a282f5daff838
Yvan Pearson <yvan.pearson@gmail.com>
2022-01-30 13:56:52 -0500

    add

--- fold_expressions2.cpp
+++ fold_expressions2.cpp
@@ -3,6 +3,8 @@
 #include <vector>
 #include <array>
 
+// https://stackoverflow.com/questions/34569455/syntax-issue-when-populating-an-array-with-a-fold-expression
+
 using namespace std;
 
 template <typename T>
@@ -23,6 +25,20 @@ struct Data
     }
 };
 
+template<class T, std::size_t N>
+struct Data1
+{
+    T arr[N]{};
+
+    template<typename... Args>
+    constexpr Data1(Args&&... pack)
+    {
+        static_assert(sizeof...(pack) <= N, "Too many args");
+        std::size_t i = 0;
+        (void(arr[i++] = pack) , ...);
+    }
+};
+
 template <int... Is>
 constexpr std::array<int, sizeof...(Is)>
 make_inc_array_impl(std::integer_sequence<int, Is...>) {
@@ -38,6 +54,8 @@ int main()
 {
 
     constexpr auto a = make_inc_array<10>(); // [1, 2, ..., 10]
+
+    constexpr Data1<int, 5> b{1, 2, 3, 4, 5};
 
     constexpr std::size_t data_len{10};
     constexpr Data<double, data_len, mathFunction> data{30.0, 40.0, 50.0, 60.0, 70.0, 80.0, 90.0, 100.0, 110.0, 120.0};
