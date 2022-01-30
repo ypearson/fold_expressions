af255ef618fe1074b544c90a714a8e2fd686c9f1
Yvan Pearson <yvan.pearson@gmail.com>
2022-01-30 13:44:31 -0500

    add

--- fold_expressions2.cpp
+++ fold_expressions2.cpp
@@ -25,11 +25,25 @@ struct Data
     }
 };
 
+template <int... Is>
+constexpr std::array<int, sizeof...(Is)>
+make_inc_array_impl(std::integer_sequence<int, Is...>) {
+    return {{(Is + 1)...}}; // +1 to start at one instead of [0, 1, ...]
+}
+
+template <std::size_t N>
+constexpr std::array<int, N> make_inc_array() {
+    return make_inc_array_impl(std::make_integer_sequence<int, N>{});
+}
+
 int main()
 {
+
+    constexpr auto a = make_inc_array<10>(); // [1, 2, ..., 10]
+
     constexpr std::size_t data_len{10};
 
-    constexpr Data<double, data_len, mathFunction> data{1, 2, 3, 4, 0};
+    constexpr Data<double, data_len, mathFunction> data{30.0, 40.0, 50.0, 60.0, 70.0, 80.0, 90.0, 100.0, 110.0, 120.0};
 
     for (size_t i{}; i < data.values.size(); i++)
         cout << data.values[i] << "\n";
